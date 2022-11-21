08324bf95c4ed4b9000ae16ef70b0ffb0c32f734
akinkayagithub <akinkaya.b@gmail.com>
2022-11-21 17:13:28 -0600

    injection

--- src/main/java/com/example/springboot_demo/student/StudentService.java
+++ src/main/java/com/example/springboot_demo/student/StudentService.java
@@ -1,17 +1,20 @@
 package com.example.springboot_demo.student;
 
+import org.springframework.stereotype.Service;
+
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+@Service
 public class StudentService {
 
     public List<Student> getStudents() {
         return new ArrayList<>(Arrays.asList(
                 new Student(1,
-                        "John",
+                        "Jane",
                         "Doe",
                         "johndoe@gmail.com",
                         LocalDate.of(2022, Month.NOVEMBER,
