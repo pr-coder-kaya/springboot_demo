3d566fffa71545e84ca80363501686d950efe23a
akinkayagithub <akinkaya.b@gmail.com>
2022-11-22 14:48:35 -0600

    added student repository and wired to student service

--- src/main/java/com/example/springboot_demo/student/StudentService.java
+++ src/main/java/com/example/springboot_demo/student/StudentService.java
@@ -1,26 +1,23 @@
 package com.example.springboot_demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class StudentService {
 
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository){
+        this.studentRepository = studentRepository;
+    }
+
+
     public List<Student> getStudents() {
-        return new ArrayList<>(Arrays.asList(
-                new Student(1,
-                        "Jane",
-                        "Doe",
-                        "johndoe@gmail.com",
-                        LocalDate.of(2022, Month.NOVEMBER,
-                                21))
-        )
-        );
+        return studentRepository.findAll();
     }
 
 }
