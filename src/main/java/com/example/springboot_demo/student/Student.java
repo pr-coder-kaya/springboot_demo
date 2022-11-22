c5fb49ac8894762f4a08227fd4d62b4d10ca6781
akinkayagithub <akinkaya.b@gmail.com>
2022-11-22 14:41:36 -0600

    created student table

--- src/main/java/com/example/springboot_demo/student/Student.java
+++ src/main/java/com/example/springboot_demo/student/Student.java
@@ -1,8 +1,22 @@
 package com.example.springboot_demo.student;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table
 public class Student {
+
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private long id;
     private String firstName;
     private String lastName;
