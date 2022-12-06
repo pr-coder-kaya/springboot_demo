13d16a1c1eb650500d7a989a4bc295480075bad1
akinkayagithub <akinkaya.b@gmail.com>
2022-12-06 12:15:28 -0600

    added return responses to delete and update services

--- src/main/java/com/example/springboot_demo/student/StudentController.java
+++ src/main/java/com/example/springboot_demo/student/StudentController.java
@@ -29,31 +29,37 @@ public class StudentController {
     }
 
     @PostMapping(path = "/create")
-    public void createStudent(@RequestBody Student student){
+    public Student createStudent(@RequestBody Student student){
         studentService.createStudent(student);
+        return student;
     }
 
     @DeleteMapping(path = "/delete/{studentId}")
-    public void deleteStudent(@PathVariable("studentId") long id){
+    public String deleteStudent(@PathVariable("studentId") long id){
         studentService.deleteStudent(id);
+        return "Student with id " + id + " is deleted.";
     }
 
     @DeleteMapping(path = "/deleteAll")
-    public void deleteAllStudents(){
+    public String deleteAllStudents(){
         studentService.deleteAllStudents();
+        if(studentService.getAllStudents().size() == 0) return "All students are deleted.";
+        throw new IllegalStateException("An issue occurred when deleting all student.");
     }
 
     @PatchMapping(path = "update/{studentId}")
-    public void updateStudent(@PathVariable("studentId") long id,
-                              @RequestParam(required = false) String firstName,
-                              @RequestParam(required = false) String lastName,
-                              @RequestParam(required = false) String email,
-                              @RequestParam(required = false)LocalDate dateOfBirth){
+    public Optional<Student> updateStudent(@PathVariable("studentId") long id,
+                                           @RequestParam(required = false) String firstName,
+                                           @RequestParam(required = false) String lastName,
+                                           @RequestParam(required = false) String email,
+                                           @RequestParam(required = false)LocalDate dateOfBirth){
         studentService.updateStudent(id, firstName, lastName, email, dateOfBirth);
+        return studentService.getStudent(id);
     }
 
     @PutMapping(path = "update/{studentId}")
-    public void updateStudent(@RequestBody Student student, @PathVariable("studentId") long id){
+    public Optional<Student> updateStudent(@RequestBody Student student, @PathVariable("studentId") long id){
         studentService.updateStudent(student, id);
+        return studentService.getStudent(id);
     }
 }
