e7be5ff9d7b176552ab7459512fe2ea485ffc412
akinkayagithub <akinkaya.b@gmail.com>
2022-12-07 13:01:41 -0600

    Fix for email and date FieldValidator methods

--- src/main/java/com/example/springboot_demo/utils/FieldValidator.java
+++ src/main/java/com/example/springboot_demo/utils/FieldValidator.java
@@ -4,9 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
-import java.time.format.ResolverStyle;
 
 public class FieldValidator {
     public static boolean isNameValid(String name) {
@@ -20,18 +20,21 @@ public class FieldValidator {
         if (email == null) throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid/missing field");
         else if (email.trim().isEmpty())
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Field value cannot be empty");
-        else if (!email.matches("[\\w._-]{2,}@[\\w._-]{2,}\\.[\\w._-]{2,}"))
+        else if (!email.matches("[\\w.-]{2,}@[\\w.-]{2,}\\.[\\w.-]{2,}"))
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid email format. The expected format is <2+chars>@<2+chars>.<2+chars> and only digits, letters, and @.-_ characters are allowed.");
         return true;
     }
 
     public static boolean isDateOfBirthValid(String date) {
         try{
-            LocalDate.parse(date, DateTimeFormatter.ofPattern("yyyy-MM-dd").withResolverStyle(ResolverStyle.STRICT));
+            LocalDate localDate = LocalDate.now(ZoneId.systemDefault());
+            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+            if(LocalDate.parse(date, dateTimeFormatter).isAfter(localDate)) throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid date. The date of birth cannot be a future date.");
         }
         catch (DateTimeParseException e){
             e.printStackTrace();
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid date format. The expected date format is yyyy-MM-dd");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid date or date format. The date should be valid and the expected date format is yyyy-MM-dd");
         }
         return true;
     }
